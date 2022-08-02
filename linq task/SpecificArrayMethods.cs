7b744be9d4cdf27b2f1de8120d74e8886e2f516e
Galina Amelina <galia.amelina@yandex.ru>
2022-08-02 16:52:38 +0300

    Add separarte class

---
+++ linq task/SpecificArrayMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_task
+{
+    public static class SpecificArrayMethodsExtensions
+    {
+        public static void ForEachEven<T>(this IEnumerable<T> numbers, Action<T> performAction)
+        {
+            var evenNumberDigits = numbers.Where((x, i) => (i + 1) % 2 == 0).ToList();
+
+            evenNumberDigits.ForEach(performAction);
+        }
+        public static void ForEachOdd<T>(this IEnumerable<T> numbers, Action<T> performAction)
+        {
+            foreach (var item in numbers.Where((x, i) => (i + 1) % 2 != 0))
+            {
+                performAction(item);
+            }
+        }
+    }
+}
